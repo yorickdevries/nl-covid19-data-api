605e9181652fbb62ec78dd80a0e169b36422fde0
Yorick de Vries <yvr@netcompany.com>
2022-08-17 18:16:33 +0200

    add swagger doc

--- app.js
+++ app.js
@@ -6,12 +6,29 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "nl-covid19-data-api",
+      version: '1.0.0',
+    },
+    basePath: "/api"
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // catch 404
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,24 @@ var express = require('express');
 var router = express.Router();
 var { latestDataJsons } = require('../downloadLatestData');
 
-/* GET json */
+/**
+ * @swagger
+ * /{jsonname}:
+ *   description: Vraag een JSON op
+ *   get:
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: jsonname
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: JSON response wanneer gevonden
+ *       404:
+ *         description: JSON niet gevonden
+ */
 router.get('/:jsonname', function(req, res, next) {
   // return json in case its present in latestDataJsons
   if (req.params.jsonname in latestDataJsons) {
