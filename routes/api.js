670519c7d5aea2ed4ddab53b8c1b039d80b23202
Yorick de Vries <yvr@netcompany.com>
2022-08-17 17:02:27 +0200

    return json in case its present

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 var { latestDataJsons } = require('../downloadLatestData');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json(latestDataJsons);
+/* GET json */
+router.get('/:jsonname', function(req, res, next) {
+  // return json in case its present in latestDataJsons
+  if (req.params.jsonname in latestDataJsons) {
+    res.json(latestDataJsons[req.params.jsonname]);
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
