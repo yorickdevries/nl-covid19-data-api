48980b8c4fc549d2b8fe0ff35967b57ebd00b345
Yorick de Vries <yvr@netcompany.com>
2022-08-17 16:28:15 +0200

    read all json files and return them in the api

--- downloadLatestData.js
+++ downloadLatestData.js
@@ -13,6 +13,8 @@ fsExtra.emptyDirSync(dataPath);
 const zipPath = path.join(dataPath, filename);
 const jsonPath = path.join(dataPath, "jsons");
 
+const latestDataJsons = {};
+
 const downloadLatestData = async () => {
   await download(API_URL, dataPath, {
     filename: filename,
@@ -21,6 +23,21 @@ const downloadLatestData = async () => {
   await decompress(zipPath, jsonPath, {
     strip: 1,
   });
+
+  console.log("Parse JSONs")
+  // read all json files
+  fsExtra.readdir(jsonPath, (err, files) => {
+    files.forEach(file => {
+      // location of the json file
+      const filePath = path.join(jsonPath, file);
+      // parse file to json object
+      const jsonObject = JSON.parse(fsExtra.readFileSync(filePath, 'utf8'));
+      // get name of the file without extension
+      const jsonName = path.parse(file).name;
+      // set jsonObject in latestDataJsons
+      latestDataJsons[jsonName] = jsonObject;
+    });
+  });
 };
 
-module.exports = { downloadLatestData, jsonPath };
+module.exports = { downloadLatestData, latestDataJsons };
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var { latestDataJsons } = require('../downloadLatestData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({"result": "OK"});
+  res.json(latestDataJsons);
 });
 
 module.exports = router;
