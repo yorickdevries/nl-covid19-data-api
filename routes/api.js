2eb6c68f01b1f9605f4df2427abc921dab410311
Yorick de Vries <yvr@netcompany.com>
2022-10-05 14:37:00 +0200

    extend with querying a field

--- routes/api.js
+++ routes/api.js
@@ -29,4 +29,39 @@ router.get('/:jsonname', function(req, res, next) {
   }
 });
 
+/**
+ * @swagger
+ * /{jsonname}/{fieldname}:
+ *   description: Vraag een veld uit een JSON op
+ *   get:
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: jsonname
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: fieldname
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: JSON field response wanneer gevonden
+ *       404:
+ *         description: JSON of field niet gevonden
+ */
+ router.get('/:jsonname/:fieldname', function(req, res, next) {
+  if (req.params.jsonname in latestDataJsons) {
+    if (req.params.fieldname in latestDataJsons[req.params.jsonname]) {
+        // return json field in case its present in latestDataJsons
+      res.json(latestDataJsons[req.params.jsonname][req.params.fieldname]);
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 module.exports = router;
